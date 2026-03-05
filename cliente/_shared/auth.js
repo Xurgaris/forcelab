d645f2fdbc2fb7eca5c6c16c6f1b9d8518f6c6d6
Xurgaris <167441372+Xurgaris@users.noreply.github.com>
2026-03-05 15:32:37 -0400

    at

--- cliente/_shared/auth.js
+++ cliente/_shared/auth.js
@@ -15,17 +15,15 @@ const firebaseConfig = {
 
   messagingSenderId: "355893469293",
   appId: "1:355893469293:web:020fbced4dd761dc9d9f64",
-};
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
-export function watchAuth(cb){
+}
+export function watchAuth(cb) {
   return onAuthStateChanged(auth, cb);
 }
 
-export async function logout(){
+export async function logout() {
   await signOut(auth);
 }
+
 export function requireAuth() {
   return new Promise((resolve) => {
     const unsub = onAuthStateChanged(auth, (user) => {
